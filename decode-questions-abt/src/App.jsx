8f86b2a6f9aefb5301a52ed3704f64ef7ec2e836
DefPavel <strikecool1@gmail.com>
2024-07-23 12:11:44 +0300

    update home

--- decode-questions-abt/src/App.jsx
+++ decode-questions-abt/src/App.jsx
@@ -1,5 +1,7 @@
 function App({ children }) {
-  return <div className="w-full h-full flex items-center justify-center">{children}</div>;
+  return (
+    <div className="w-full h-full flex items-center justify-center bg-gray-50">{children}</div>
+  );
 }
 
 export default App;
--- decode-questions-abt/src/components/HomePage/HomePage.jsx
+++ decode-questions-abt/src/components/HomePage/HomePage.jsx
@@ -1,20 +1,34 @@
 import { NavLink } from 'react-router-dom';
 
 export const HomePage = () => {
-  const links = [{ to: 'questions', label: 'Получить вопросы' }];
+  const links = [
+    { to: 'questions', label: 'Получить вопросы по pers_test' },
+    { to: 'questions', label: 'Пофиксить все ошибки' },
+    { to: 'questions', label: 'Обнулить тесты' },
+    { to: 'questions', label: 'Уничтожить человека' },
+    { to: 'questions', label: 'Что-то еще' },
+  ];
 
   return (
-    <div className="grid grid-cols-4 justify-evenly bg-slate-400 w-[80%] h-[80%] rounded-xl p-4">
-      {links.map((el, id) => (
-        <div
-          key={id}
-          className="grid w-[200px] h-[100px] bg-slate-200 hover:bg-slate-300 transition duration-300 rounded-xl items-center justify-self-center"
-        >
-          <NavLink className="grid w-full h-full items-center justify-center" to={el.to}>
-            <p className="text-xl">{el.label}</p>
-          </NavLink>
-        </div>
-      ))}
+    <div className="w-[70%] h-[80%] p-4 bg-white dark:bg-gray-600 rounded-xl shadow-lg">
+      <div className="flex justify-center mb-6">
+        <h3 className="text-3xl font-semibold text-gray-900 dark:text-gray-100">
+          Навигационная панель
+        </h3>
+      </div>
+      <div className="p-6 border-t border-blue-gray-50"></div>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {links.map((el, id) => (
+          <div
+            key={id}
+            className="flex items-center justify-center w-full h-[100px] py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          >
+            <NavLink className="text-center" to={el.to}>
+              <p className="text-xl">{el.label}</p>
+            </NavLink>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
